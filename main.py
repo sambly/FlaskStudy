75775dac02dd87f783bf94b66abfa3adbe19decc
Евгений <45079052+sambly@users.noreply.github.com>
2022-06-22 23:08:56 +0300

    1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import  Flask, render_template
+from flask import  Flask, render_template, url_for
 
 
 
@@ -10,13 +10,25 @@ menu = ["Установка","Первое приложение","Обратна
 
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html',title ='Про Flask',menu=menu)
 
 
 @app.route("/about")
 def about():
-    return render_template()
+    return render_template('index.html',title ='Про Flask',menu=menu)
 
 
+@app.route("/profile/<path:username>/<path>")
+def profile(username,path):
+    return  f"Пользователь : {username},{path}"
+
+
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile',username="abc",path='her'))
+# #
 if __name__=="__main__":
     app.run(debug=True)
